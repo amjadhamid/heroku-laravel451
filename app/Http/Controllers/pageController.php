0a1931db4eee8013bc0a651783b306f6b0580812
amjadhamid <majdsy451@gmail.com>
2018-04-15 15:09:10 +0300

    basics knowlege about Router and controller

---
+++ app/Http/Controllers/pageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pageController extends Controller
+{
+
+
+
+public function index() {
+return view("pages.index");
+}
+
+
+
+
+
+
+
+
+
+}
