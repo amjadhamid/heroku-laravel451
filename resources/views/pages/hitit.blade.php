66524ee5f049ee635bc6df94a5d0c42c61c09f52
amjadhamid <majdsy451@gmail.com>
2018-04-15 15:40:16 +0300

    templates balde

--- resources/views/pages/hitit.blade.php
+++ resources/views/pages/hitit.blade.php
@@ -1,1 +1,12 @@
-{{'hitit'}}
+@extends('layout.app')
+@section('content')
+    
+
+            <div class="content">
+                <div class="title m-b-md">
+            hitit
+           
+                </div>
+    </div>
+
+    @endsection
