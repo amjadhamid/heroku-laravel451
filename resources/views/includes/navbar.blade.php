07570b98399576662373793bf03b6f7e5e8febb5
amjadhamid <majdsy451@gmail.com>
2018-04-16 21:59:17 +0300

    add Create post t othe navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -27,6 +27,9 @@
       <li class="nav-item">
         <a class="nav-link" href="/about">About</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/posts/create">Create Post</a>
+      </li>
     </ul>
    
    <ul class ="nav navbar-nav navbar-right">
