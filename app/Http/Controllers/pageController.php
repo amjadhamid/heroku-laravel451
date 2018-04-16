42a0aaf8ce8fccbebd5006ec764c9a1ae2f3c6e1
amjadhamid <majdsy451@gmail.com>
2018-04-16 18:34:16 +0300

    make the database then create the module and controller and migrate to database then creatpostcontroller and route to it

--- app/Http/Controllers/pageController.php
+++ app/Http/Controllers/pageController.php
@@ -15,7 +15,23 @@ public function index() {
     $name='index';
 return view("pages.index" )->with('name',$name);
 }
-
+public function database() {
+    // Schema::create('networks', function(Blueprint  $table)
+    // {
+    //     $table->increments('id');
+    //     $table -> string('name' ,25);
+    //     $table -> string('lastname',50);
+    //     $table -> string('description',50);
+     
+    //     $table -> date('created');
+    //     $table -> string('fullname');
+    //     $table -> timestamps();
+    // });
+    Schema::create('testusers', function($table)
+    {
+        $table->increments('id');
+    });
+}
 
 public function about() {
     $name='about';
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/' , function () {
+//     Schema::create('users', function($table)
+//     {
+//         $table->increments('id');
+//     });
+
+//     return view('welcome');
+// });
+    // Route::get('/' , 'pageController@database' );
+ Route::resource('posts' , 'PostController' );
+      
 
 Route::get('/' , 'pageController@index' );
 
@@ -18,3 +29,21 @@ Route::get('/index' , 'pageController@index' );
 Route::get('/about' , 'pageController@about' );
 
 Route::get('/hitit' , 'pageController@hitit' );
+
+
+
+    // Schema::create('users', function ( $table) {
+    //     $table->increments('id');
+    //     $table->string('name');
+    //     $table->string('email')->unique();
+    //     $table->string('password');
+    //     $table->rememberToken();
+    //     $table->timestamps();
+    // },
+    //     Schema::create('password_resets', function ( $table) {
+    //         $table->string('email')->index();
+    //         $table->string('token');
+    //         $table->timestamp('created_at')->nullable();
+    //     })
+// return view('WELCOME')
+//      )};
